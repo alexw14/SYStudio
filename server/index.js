6411dfcfb120b1e95fe04db880c0f119a5783f3e
Alex Wong <alexw14@gmail.com>
2021-09-28 21:01:40 +0800

    Set up MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+  if (err) throw err;
+  console.log('Connected to MongoDB!');
+});
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
