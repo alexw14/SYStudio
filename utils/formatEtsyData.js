7e36ec18da19c0666a22770732d887d3e8f24213
Alex Wong <alexw14@gmail.com>
2021-09-30 21:55:28 +0800

    add format etsy data helper function

---
+++ utils/formatEtsyData.js
@@ -0,0 +1,39 @@
+const formatEtsyData = (soldOrders, soldOrderItems) => {
+  const soldOrdersData = [];
+  const soldOrderItemsMap = {};
+
+  // Generate Sold Order Items Map
+  for (let item of soldOrderItems) {
+    let orderId = item['Order ID'];
+    if (!soldOrderItemsMap[orderId]) {
+      soldOrderItemsMap[orderId] = [];
+    }
+    soldOrderItemsMap[orderId].push(item);
+  }
+
+  // Format Sold Order Data
+  for (let order of soldOrders) {
+    const orderObj = {};
+    orderObj.orderId = order['Order ID'];
+    orderObj.saleDate = order['Sale Date'];
+    orderObj.soldItems = soldOrderItemsMap[order['Order ID']];
+    orderObj.numOfItems = order['Number of Items'];
+    orderObj.orderNet = order['Order Net'];
+    orderObj.orderValue = order['Order Value'];
+    orderObj.shippingCharged = order['Shipping'];
+    orderObj.shippingTransactionFees = parseFloat(
+      (0.05 * order['Shipping']).toFixed(2)
+    );
+    orderObj.paymentProcessFees = order['Card Processing Fees'];
+    orderObj.listingFees = parseFloat(
+      (0.2 * order['Number of Items']).toFixed(2)
+    );
+    orderObj.discountAmount = order['Discount Amount'];
+    orderObj.adjustedOrderNet = order['Adjusted Net Order Amount'];
+    orderObj.adjustedPaymentProcessFees =
+      order['Adjusted Card Processing Fees'];
+    soldOrdersData.push(orderObj);
+  }
+
+  return soldOrdersData;
+};
