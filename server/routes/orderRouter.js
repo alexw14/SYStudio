c77ead58506b3358bbdb338562a2e8f9f5226705
Alex Wong <alexw14@gmail.com>
2021-10-11 23:33:59 +0800

    update orderRouter to include adding order history to DB

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -2,25 +2,42 @@ const express = require('express');
 const router = express.Router();
 
 const Order = require('../models/Order');
+const OrderHistory = require('../models/OrderHistory');
 
 router.post('/', async (req, res) => {
   try {
     const newOrder = new Order(req.body);
     const foundOrder = await Order.findOne({ orderId: newOrder.orderId });
-    if (foundOrder) {
-      return res.json({
-        success: false,
-        message: 'This order already exists in the database',
-      });
-    } else {
+    const foundOrderHistory = await OrderHistory.findOne({
+      monthAndYear: newOrder.saleMonthAndYear,
+    });
+
+    if (!foundOrder) {
       const savedOrder = await newOrder.save();
+
+      if (foundOrderHistory) {
+        foundOrderHistory.orders.push(savedOrder._id);
+        foundOrderHistory = await foundOrderHistory.save();
+      } else {
+        const newOrderHistory = new OrderHistory({
+          monthAndYear: newOrder.saleMonthAndYear,
+          orders: [savedOrder._id],
+        });
+        const savedOrderHistory = await newOrderHistory.save();
+      }
+
       return res.status(200).json({
         success: true,
         order: savedOrder,
       });
+    } else {
+      return res.json({
+        success: false,
+        message: 'This order already exists in the database',
+      });
     }
   } catch (err) {
-    return res.json({ success: false, err });
+    return res.json({ success: false, error: err });
   }
 });
 
